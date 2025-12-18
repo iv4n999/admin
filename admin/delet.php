a6b459eaef98d2ded01fda40c8c744436cce2f21
iv4n999 <158744825+iv4n999@users.noreply.github.com>
2025-12-18 20:02:50 +0300

    Update delet.php

--- admin/delet.php
+++ admin/delet.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * Удаление отгрузки
+ */
 
+require_once '../includes/functions.php';
+requireAuth();
+
+$id = (int)($_GET['id'] ?? 0);
+
+if ($id && deleteShipment($id)) {
+    header('Location: index.php?success=Отгрузка удалена');
+} else {
+    header('Location: index.php?error=Ошибка при удалении');
+}
+exit;
