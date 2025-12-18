1904dc0b77e67a45d63c17782d113f64f2585a03
iv4n999 <158744825+iv4n999@users.noreply.github.com>
2025-12-18 20:05:33 +0300

    Update delete-city.php

--- admin/delete-city.php
+++ admin/delete-city.php
@@ -1,1 +1,12 @@
+<?php
+require_once '../includes/functions.php';
+requireAuth();
 
+$id = (int)($_GET['id'] ?? 0);
+
+if ($id && deleteCity($id)) {
+    header('Location: cities.php?success=Город удалён');
+} else {
+    header('Location: cities.php?error=Ошибка при удалении');
+}
+exit;
